ee3d1a98b0d5cf6be5869c6b565df86d11506c58
actoract <annchuprina166@yandex.ru>
2021-05-25 03:30:34 +0300

    Updated N

--- frontend/src/screens/UserScreen.js
+++ frontend/src/screens/UserScreen.js
@@ -1,21 +1,51 @@
 import React, {useState, useEffect} from 'react'
-import {Row, Col} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Loader from '../components/loader'
 import {userOrdersAction} from '../actions/orderActions'
 import {getProfileAction} from '../actions/userAction.js'
 import { useTranslation } from 'react-i18next'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Popover,  } from 'antd';
+import { Popover, Col, Row } from 'antd';
 import Message from '../components/message'
 
 const UserScreen = ({location, history}) => {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordConfirm, setPasswordConf] = useState('')
-    const [mes, setMess] = useState(null)
+
+    const style1 = {  
+        position: 'relative',
+        width: 'auto',
+        height: 'auto',
+        top: '0',
+        maxHeight: '1000px',
+        padding: '20px',
+        marginBottom: '10px',
+        borderRadius: '5px',
+        borderWidth: '10px',
+        backdropFilter: '1px',
+        backgroundColor: 'white',
+        boxShadow: '0 6px 15px rgb(36 37 38 / 8%)',
+    };
+    const style2={  
+        textAlign: 'left',
+        fontSize: '16px',
+        marginTop: '20px',
+        margin: '20px',
+        padding: '20px',
+    }
+    const style3 = {  
+        position: 'relative',
+        width: 'auto',
+        height: 'auto',
+        top: '0',
+        maxHeight: '1000px',
+        padding: '20px',
+        marginBottom: '10px',
+        borderRadius: '5px',
+        borderWidth: '10px',
+        backdropFilter: '1px',
+        backgroundColor: 'white',
+        boxShadow: '0 6px 15px rgb(36 37 38 / 8%)',
+        marginTop: '30px',
+      };
 
     const { t } = useTranslation()
     const dispatch = useDispatch()
@@ -46,68 +76,57 @@ const UserScreen = ({location, history}) => {
                 //dispatch({ type: USER_UPDATE_PROFILE_RESET })
                 dispatch(getProfileAction('profile'))
                 dispatch(userOrdersAction())
-            } else {
-                setFirstName(user.firstName)
-                setLastName(user.lastName)
-                setEmail(user.email)
-            }
+            } 
         }
     }, [dispatch, history, userDet, user])
 
-    /*const submitHandler = (e) =>{
-        e.preventDefault()
-        /*if(password && password !== passwordConfirm){
-            message.error(t('Passwods dont match.1'), 3);
-        }
-            dispatch(updProfileAction({id: user._id, firstName, lastName, email, password}))
-        if (success){
-            message.success(t('Success.1'), 1)
-        }
-    }*/
     return (
         <>{userDet &&
-            <Row>
-                <Col md={3}>
-                    <div className = "CardDetails2">
-                        <p><strong>{t('First name.1')}:</strong> {userDet.firstName}</p>
-                        <p><strong>{t('Last name.1')}:</strong> {userDet.lastName}</p>
-                        <p><strong>{t('Email.1')}:</strong> {userDet.email}</p>
-                    </div>
-                </Col>
-                <Col md = {9}>
-                    <Row className = "text_details" key = "header">
-                        <Col md={6}>{t('ID.1')}</Col>
-                        <Col md={2}>{t('Total price.1')}</Col>
-                        <Col md={2}>{t('Paid.1')}</Col>
-                        <Col md={2}>{t('Delivered.1')}</Col>
-                    </Row>
-                        
-                    {loadingOrders ? <Loader loadingVal = {loadingOrders}/> : 
-                    errorOrders ? <Message>{errorOrders}</Message> :
-                    !orders ? 
-                        <h1>t('ORDER LIST IS EMPTY.1')</h1>:
-                        <>
-                        {orders.map(item => (
-                            <LinkContainer to={`/order/${item._id}`}>
-                                <Popover content={messageOnHover} title="How to view order details?" key={item._id}> 
-                                <Row key={item._id} className = "CardDetails2 details">
-                                    <Col md={4}>{item._id}</Col>
-                                    <Col md={2}>{item.user && item.user.firstName} {item.user && item.user.lastName}</Col>
-                                    <Col md={2}>{item.totalPrice && item.totalPrice}</Col>
-                                    {
-                                        item.isPaid == false ? <Col md={2}> <strong>✕</strong></Col> : <Col md={2}> <strong> ✓ </strong></Col>
-                                    }
-                                    {
-                                        item.isDelivered == false ? <Col md={2}> <strong>✕</strong></Col> : <Col md={2}> <strong> ✓ </strong></Col>
-                                    }
-                                </Row>
-                                </Popover>
-                            </LinkContainer>
-                        ))}
-                        </>
-                    }
-                </Col>
-            </Row>
+            <>
+                <Row>
+                    <Col span={4}>
+                        <div style = {style1}>
+                            <p><strong>{t('First name.1')}:</strong> {userDet.firstName}</p>
+                            <p><strong>{t('Last name.1')}:</strong> {userDet.lastName}</p>
+                            <p><strong>{t('Email.1')}:</strong> {userDet.email}</p>
+                        </div>
+                    </Col>
+                    <Col span={18}key = "header" style={style2}>
+                        <Row>
+                            <Col span={9}>{t('ID.1')}</Col>
+                            <Col span={4}>{t('Total price.1')}</Col>
+                            <Col span={4}>{t('Paid.1')}</Col>
+                            <Col span={4}>{t('Delivered.1')}</Col>
+
+                            <Col span = {24}>                        
+                            {loadingOrders ? <Loader loadingVal = {loadingOrders}/> : 
+                            errorOrders ? <Message>{errorOrders}</Message> :
+                            !orders ? 
+                                <h1>t('ORDER LIST IS EMPTY.1')</h1>:
+                                <>
+                                {orders.map(item => (
+                                    <LinkContainer to={`/order/${item._id}`}>
+                                        <Popover content={messageOnHover} title="How to view order details?" key={item._id}> 
+                                        <Row key={item._id} style={style3}>
+                                            <Col span={9}>{item._id}</Col>
+                                            <Col span={4}>{item.totalPrice && item.totalPrice}</Col>
+                                            {
+                                                item.isPaid == false ? <Col span={4}> <strong>✕</strong></Col> : <Col span={4}> <strong> ✓ </strong></Col>
+                                            }
+                                            {
+                                                item.isDelivered == false ? <Col span={4}> <strong>✕</strong></Col> : <Col span={4}> <strong> ✓ </strong></Col>
+                                            }
+                                        </Row>
+                                        </Popover>
+                                    </LinkContainer>
+                                ))}
+                                </>
+                            }
+                        </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </>
         }
         </>
     )
