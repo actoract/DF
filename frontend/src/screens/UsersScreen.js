be68b9d4a4e558684a23ef24649d6a5ad3dd6a95
actoract <annchuprina166@yandex.ru>
2021-05-25 03:39:09 +0300

    Updated N

--- frontend/src/screens/UsersScreen.js
+++ frontend/src/screens/UsersScreen.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect, Suspnse} from 'react'
+import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import Loader from '../components/loader'
 import { useTranslation } from 'react-i18next'
-//import {Col, Row} from 'react-bootstrap'
-import { Popover, message,  Row, Col  } from 'antd';
+import {Col, Row} from 'react-bootstrap'
+import { Popover, message } from 'antd';
 import './styles.css'
 import ModalMessage from "../components/modalMessage"
 import {USER_PROFILE_UPD_RESET} from '../constants/storeConst'
@@ -13,31 +13,7 @@ import {getProfileAction,
 
 const Message = React.lazy(() => import('../components/message'));
 
-
-
 const UsersScreen = ({history, match}) => {
-    const style1 = {
-        textAlign: 'left',
-        fontSize: '16px',
-        margin: '30px',
-    };
-    
-    const style2 = {  
-      position: 'relative',
-      width: 'auto',
-      height: 'auto',
-      top: '0',
-      maxHeight: '1000px',
-      padding: '20px',
-      marginBottom: '10px',
-      borderRadius: '5px',
-      borderWidth: '10px',
-      backdropFilter: '1px',
-      backgroundColor: 'white',
-      boxShadow: '0 6px 15px rgb(36 37 38 / 8%)',
-      marginTop: '20px',
-      margin: '20px'
-    };
     const userId = match.params.id
     const [isAdmin, setAdmin] = useState(false);
     const dispatch = useDispatch()
@@ -48,10 +24,10 @@ const UsersScreen = ({history, match}) => {
     const {userDet} = userLogin
 
     const userProfile = useSelector(state => state.userProfile)
-    const {user} = userProfile
+    const {user, loading: loadingProfile, error: errorProfile} = userProfile
 
     const userUpdProfile = useSelector(state => state.userUpdProfile)
-    const {success: UpdSucccess} = userUpdProfile
+    const {success: UpdSucccess, loading: loadingUpdProfile, error: errorUpdProfile} = userUpdProfile
    
     const [isModalVis, setIsModalVisible] = useState(false);
     const messageAdminOnHover = (
@@ -65,6 +41,7 @@ const UsersScreen = ({history, match}) => {
         if(UpdSucccess){
             setAdmin(user.isAdmin)
             dispatch({type: USER_PROFILE_UPD_RESET})
+            //history.push()
         }
         else{
             if(user.name && user._id !== userId){
@@ -89,6 +66,7 @@ const UsersScreen = ({history, match}) => {
         e.preventDefault()
         if(userDet._id != id){
             setAdmin(e.target.value)
+            setIdChange(id)
             dispatch(updProfileAction({_id: id, isAdmin}))
         }
         else{
@@ -100,20 +78,20 @@ const UsersScreen = ({history, match}) => {
            <h1>{t("Users.1")}</h1> 
            {loading ? <Loader loadingVal = {loading}/>: error ? <Message>{error}</Message>  : 
            <>
-           <Row  style={style1} key = "header">
-                <Col span={4}><strong>{t('ID.1')}</strong></Col> 
-                <Col span={2}><strong>{t('First name.1')}</strong></Col> 
-                <Col span={2}><strong>{t('Last name.1')}</strong></Col> 
-                <Col span={2}><strong>{t('Email.1')}</strong></Col> 
-                <Col span={2}><strong>{t('Admin status.1')}</strong></Col> 
+           <Row  className = "text_details" key = "header">
+                <Col md={4} className =""><strong>{t('ID.1')}</strong></Col> 
+                <Col md={2} className =""><strong>{t('First name.1')}</strong></Col> 
+                <Col md={2} className =""><strong>{t('Last name.1')}</strong></Col> 
+                <Col md={2} className =""><strong>{t('Email.1')}</strong></Col> 
+                <Col md={2} className =""><strong>{t('Admin status.1')}</strong></Col> 
             </Row>
            {users.map(item => (
                <Popover content={messageAdminOnHover} title="How to update user status?" key={item._id}> 
-                <Row key={users.id}  style={style2} onClick = {e => handleStatusChange(e, item._id)}>
-                    <Col span={4}>{item._id}</Col>
-                    <Col span={2}>{item.firstName}</Col>
-                    <Col span={2}>{item.lastName}</Col>
-                    <Col span={2}>{item.email}</Col>
+                <Row key={users.id} className = "CardDetails2 details" onClick = {e => handleStatusChange(e, item._id)}>
+                    <Col md={4}>{item._id}</Col>
+                    <Col md={2}>{item.firstName}</Col>
+                    <Col md={2}>{item.lastName}</Col>
+                    <Col md={2}>{item.email}</Col>
                     {
                         item.isAdmin == false ? <Col md={2}> <strong>✕</strong></Col> : <Col md={2}> <strong> ✓ </strong></Col>
                     }
