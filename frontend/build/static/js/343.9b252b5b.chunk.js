0d7bde3ac7e13124a02dfafd2e31c606962d1c28
annachuprina <annchuprina166@yandex.ru>
2023-03-28 15:13:40 +0200

    1st commit

---
+++ frontend/build/static/js/343.9b252b5b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[343],{4343:function(e,n,a){a.r(n),a.d(n,{default:function(){return i}});a(2791);var s=a(1523),t=a(8735),c=a(3383),r=a(184),i=(t.Z.Text,c.Z.Meta,function(e){var n=e.product;return(0,r.jsx)(s.Link,{to:"/products/".concat(n._id),children:(0,r.jsxs)("div",{className:"Card",children:[(0,r.jsx)("img",{className:"img",alt:"WEARDROP",src:n.image}),(0,r.jsxs)("div",{className:"text",children:[n.name.nameRus," / ",n.name.nameEng]})]})})})}}]);
+//# sourceMappingURL=343.9b252b5b.chunk.js.map
---
+++ frontend/build/static/js/384.dc07d16f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[384],{4384:function(e,n,s){s.r(n),s.d(n,{default:function(){return i}});s(2791);var t=s(1523),a=s(8735),c=s(3383),r=s(184),i=(a.Z.Text,c.Z.Meta,function(e){var n=e.testproducts;return(0,r.jsx)(t.Link,{to:"/testproducts/".concat(n._id),children:(0,r.jsxs)("div",{className:"Card",children:[(0,r.jsx)("img",{className:"img",alt:"WEARDROP",src:n.image}),(0,r.jsxs)("div",{className:"text",children:[n.name.nameRus,"/",n.name.nameEng]})]})})})}}]);
+//# sourceMappingURL=384.dc07d16f.chunk.js.map
